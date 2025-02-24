f99fe91044e6014532eecdd5f07023b5bc5b81af
dbenfon341 <dbenfon341@g.educaand.es>
2025-02-24 03:12:11 +0100

    jefe done

--- src/menus/menu_jefe.py
+++ src/menus/menu_jefe.py
@@ -1,34 +1,34 @@
 from src.interfaz.consola import *
 from src.interfaz.interface_strings import *
-
+from src.sistema.jefes import crear_grupo_tareas, agregar_empleado, gestionar_tareas
+from src.utils.utils import *
 
 def jefe_menu():
     salir = False
 
+    nombre = input(STRING_ASK_NAME)
+    pausar_programa(1)
+
     while not salir:
-        nombre = input(STRING_ASK_NAME)
-        pausar_programa(1)
         mostrar_menu_master(nombre)
         opcion = pedir_numero(STRING_GLOBAL_OPCION)
-        if opcion == 5:
+        if opcion == 4:
             print(STRING_EXIT)
             salir = True
         else:
             if opcion == 1:
                 print(MENU_2_JEFE_1)
-                nombre_empleado = input(MENU_2_JEFE_2)
-                mensaje_carga_datos()
-                salir = True
+                crear_grupo_tareas()
+                pausar_programa(3)
             elif opcion == 2:
-                mostrar_menu_empleados()
+                print(MENU_2_JEFE_1)
+                mensaje_carga_datos()
+                gestionar_tareas()
                 pausar_programa(3)
             elif opcion == 3:
-                nombre_empleado = input(MENU_1_JEFE_1)
-                dni = pedir_numero(MENU_1_JEFE_2)
-                mensaje_carga_datos()
-                salir = True
-            elif opcion == 4:
-                salir = True
+                print(MENU_2_JEFE_1)
+                agregar_empleado()
+                pausar_programa(3)
             else:
                 print("Opción no válida")
                 pausar_programa(3)
